df5021bd13f52700a1222ecbd05b2746533d01ad
Milad Fadavvi <Fadavvi@users.noreply.github.com>
2026-09-03 18:09:24 +0000

    crt.name added

--- Sub-Drill.sh
+++ Sub-Drill.sh
@@ -40,6 +40,7 @@ curl --silent --insecure --tcp-fastopen --tcp-nodelay "https://certificatedetail
 curl --silent --insecure --tcp-fastopen --tcp-nodelay "https://columbus.elmasy.com/report/$1" | grep -o -E "[a-zA-Z0-9._-]+\.$1" >> /tmp/sub-drill-tmp.txt &
 curl --silent --insecure --tcp-fastopen --tcp-nodelay "https://api.subdomain.center/?domain=$1&engine=cuttlefish" | grep -o -E "[a-zA-Z0-9._-]+\.$1" >> /tmp/sub-drill-tmp.txt &
 curl --silent --insecure --tcp-fastopen --tcp-nodelay "https://ip.thc.org/api/v1/subdomains/download?domain=$1&limit=50000" | grep -o -E "[a-zA-Z0-9._-]+\.$1" >> /tmp/sub-drill-tmp.txt &
+curl --silent --insecure --tcp-fastopen --tcp-nodelay "https://crt.name/v1/search?apex=$1" >> /tmp/sub-drill-tmp.txt &
 wait
 
 if [[ $# -eq 2 ]]; then
